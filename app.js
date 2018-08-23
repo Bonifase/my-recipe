5470349835a20204217c0914a75b4eed1d13c231
Bonifase Orwa <orwabonifase@gmail.com>
2018-08-23 20:50:38 +0300

    add navbar

--- app.js
+++ app.js
@@ -3,25 +3,68 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var bodyParser = require('boby-parser');
+var bodyParser = require('body-parser');
 var session = require('express-session');
 var passport = require('passport');
 var favicon = require('serve-favicon');
+var localStrategy = require('passport-local').Strategy;
+var multer = require('multer');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var pg = require("pg");
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+var connect = "postgresql://bonifaseorwa:@localhost/recipe";
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+// handle file uploads
+var upload =multer({dest:'./uploads'});
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// handle sessions
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+// passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// validator
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value) {
+        var namespace = param.split('.')
+        , root    = namespace.shift()
+        , formParam = root;
+  
+      while(namespace.length) {
+        formParam += '[' + namespace.shift() + ']';
+      }
+      return {
+        param : formParam,
+        msg   : msg,
+        value : value
+      };
+    }
+  }));
+
+// flash messages
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
